e9b6d1a485f66e81887a094099af5ca47136bf6c
Ariel Gallardo <ariel.gallardo.dev@gmail.com>
2024-11-12 15:37:51 -0300

    01-Producto-Pedido# Producto nullable properties

--- Restaurante.Migrations/Migrations/20241112183436_ProductoIngredienteUpdate_3.Designer.cs
+++ Restaurante.Migrations/Migrations/20241112183436_ProductoIngredienteUpdate_3.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Restaurante.Migrations;
 
@@ -11,9 +12,10 @@ using Restaurante.Migrations;
 namespace Restaurante.Migrations.Migrations
 {
     [DbContext(typeof(RestauranteContext))]
-    partial class RestauranteContextModelSnapshot : ModelSnapshot
+    [Migration("20241112183436_ProductoIngredienteUpdate_3")]
+    partial class ProductoIngredienteUpdate_3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Restaurante.Migrations/Migrations/20241112183436_ProductoIngredienteUpdate_3.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurante.Migrations.Migrations
+{
+    public partial class ProductoIngredienteUpdate_3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "usuarios",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 11, 12, 18, 34, 35, 566, DateTimeKind.Utc).AddTicks(7026),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 11, 12, 17, 20, 3, 993, DateTimeKind.Utc).AddTicks(9235));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "telefonos",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 11, 12, 18, 34, 35, 566, DateTimeKind.Utc).AddTicks(3408),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 11, 12, 17, 20, 3, 993, DateTimeKind.Utc).AddTicks(6361));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "roles",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 11, 12, 18, 34, 35, 565, DateTimeKind.Utc).AddTicks(9945),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 11, 12, 17, 20, 3, 993, DateTimeKind.Utc).AddTicks(2984));
+
+            migrationBuilder.AlterColumn<double>(
+                name: "stock_alerta",
+                table: "productos",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "stock_actual",
+                table: "productos",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "precio_venta",
+                table: "productos",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "precio_compra",
+                table: "productos",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "productos",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 11, 12, 18, 34, 35, 567, DateTimeKind.Utc).AddTicks(4395),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 11, 12, 17, 20, 3, 994, DateTimeKind.Utc).AddTicks(5101));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "producto_ingrediente",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 11, 12, 18, 34, 35, 567, DateTimeKind.Utc).AddTicks(2269),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 11, 12, 17, 20, 3, 994, DateTimeKind.Utc).AddTicks(3443));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "personas",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 11, 12, 18, 34, 35, 566, DateTimeKind.Utc).AddTicks(5191),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 11, 12, 17, 20, 3, 993, DateTimeKind.Utc).AddTicks(7757));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "pedidos",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 11, 12, 18, 34, 35, 567, DateTimeKind.Utc).AddTicks(8565),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 11, 12, 17, 20, 3, 994, DateTimeKind.Utc).AddTicks(8544));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "ingredientes",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 11, 12, 18, 34, 35, 566, DateTimeKind.Utc).AddTicks(9699),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 11, 12, 17, 20, 3, 994, DateTimeKind.Utc).AddTicks(1319));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "domicilios",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 11, 12, 18, 34, 35, 566, DateTimeKind.Utc).AddTicks(1968),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 11, 12, 17, 20, 3, 993, DateTimeKind.Utc).AddTicks(4879));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "detalle_pedido",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 11, 12, 18, 34, 35, 567, DateTimeKind.Utc).AddTicks(6915),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 11, 12, 17, 20, 3, 994, DateTimeKind.Utc).AddTicks(7185));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "usuarios",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 11, 12, 17, 20, 3, 993, DateTimeKind.Utc).AddTicks(9235),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 11, 12, 18, 34, 35, 566, DateTimeKind.Utc).AddTicks(7026));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "telefonos",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 11, 12, 17, 20, 3, 993, DateTimeKind.Utc).AddTicks(6361),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 11, 12, 18, 34, 35, 566, DateTimeKind.Utc).AddTicks(3408));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "roles",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 11, 12, 17, 20, 3, 993, DateTimeKind.Utc).AddTicks(2984),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 11, 12, 18, 34, 35, 565, DateTimeKind.Utc).AddTicks(9945));
+
+            migrationBuilder.AlterColumn<double>(
+                name: "stock_alerta",
+                table: "productos",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "stock_actual",
+                table: "productos",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "precio_venta",
+                table: "productos",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "precio_compra",
+                table: "productos",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "productos",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 11, 12, 17, 20, 3, 994, DateTimeKind.Utc).AddTicks(5101),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 11, 12, 18, 34, 35, 567, DateTimeKind.Utc).AddTicks(4395));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "producto_ingrediente",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 11, 12, 17, 20, 3, 994, DateTimeKind.Utc).AddTicks(3443),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 11, 12, 18, 34, 35, 567, DateTimeKind.Utc).AddTicks(2269));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "personas",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 11, 12, 17, 20, 3, 993, DateTimeKind.Utc).AddTicks(7757),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 11, 12, 18, 34, 35, 566, DateTimeKind.Utc).AddTicks(5191));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "pedidos",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 11, 12, 17, 20, 3, 994, DateTimeKind.Utc).AddTicks(8544),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 11, 12, 18, 34, 35, 567, DateTimeKind.Utc).AddTicks(8565));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "ingredientes",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 11, 12, 17, 20, 3, 994, DateTimeKind.Utc).AddTicks(1319),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 11, 12, 18, 34, 35, 566, DateTimeKind.Utc).AddTicks(9699));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "domicilios",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 11, 12, 17, 20, 3, 993, DateTimeKind.Utc).AddTicks(4879),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 11, 12, 18, 34, 35, 566, DateTimeKind.Utc).AddTicks(1968));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "detalle_pedido",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 11, 12, 17, 20, 3, 994, DateTimeKind.Utc).AddTicks(7185),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 11, 12, 18, 34, 35, 567, DateTimeKind.Utc).AddTicks(6915));
+        }
+    }
+}

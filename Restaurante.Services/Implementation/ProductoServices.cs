6c0f85fd502eea3f9baee0fe68e7dfd2be4f7f76
Ariel Gallardo <ariel.gallardo.dev@gmail.com>
2024-11-15 21:50:53 -0300

    01-Producto-Pedido# Add - Edit - Delete (Producto)

--- Restaurante.Services/Implementation/ProductoServices.cs
+++ Restaurante.Services/Implementation/ProductoServices.cs
@@ -20,18 +20,18 @@ namespace Restaurante.Services
         {
             var result = new ResultResponse();
             var newProduct = _mappper.Map<CrearProductoDTO, Producto>(dTO);
-            if(await _unitOfWork.Producto.Where(x => EF.Functions.Like(x.Nombre, dTO.Nombre)).CountAsync() < 1)
+            if(await _unitOfWork.Producto.Where(x => EF.Functions.Like(x.Nombre, dTO.Nombre) && x.DeletedAt == null).CountAsync() < 1)
             {
                 await _unitOfWork.Producto.Insert(newProduct);
-                foreach (var i in newProduct.Ingredientes)
-                {
-
-                }
+                await _unitOfWork.ProductoIngrediente.Insert(newProduct.Ingredientes);
                 await _unitOfWork.SaveChangesAsync();
+                result.Message = $@"ENTITY_CREATED ""PRODUCT,{newProduct.Nombre}""";
+                result.StatusCode = 200;
             }
             else
             {
-
+                result.Message = $@"ENTITY_EXISTS ""PRODUCT,{newProduct.Nombre}""";
+                result.StatusCode = 400;
             }
             return result;
         }
@@ -40,15 +40,21 @@ namespace Restaurante.Services
         {
             var result = new ResultResponse();
             var srcProduct = _mappper.Map<EditarProductoDTO, Producto>(dTO);
-            var currentProduct = _unitOfWork.Producto.Where(x => x.Id == dTO.ProductoId).FirstOrDefault();
+            var currentProduct = _unitOfWork.Producto.WhereActive(x => x.Id == dTO.ProductoId && x.DeletedAt == null).Take(1).FirstOrDefault();
             if(currentProduct != null)
             {
-                currentProduct = _mappper.Map(srcProduct, currentProduct);
-
+                var newOptions = _mappper.Map(srcProduct, currentProduct);
+                _unitOfWork.Producto.Update(newOptions);
+                _unitOfWork.ProductoIngrediente.Update(newOptions.Ingredientes.Where(x => currentProduct.Ingredientes.Contains(x)));
+                _unitOfWork.ProductoIngrediente.Delete(currentProduct.Ingredientes.Where(x => !newOptions.Ingredientes.Contains(x) && x.CreatedAt != null));
+                await _unitOfWork.ProductoIngrediente.Insert(newOptions.Ingredientes.Where(x => !currentProduct.Ingredientes.Contains(x) && x.CreatedAt == null));
+                await _unitOfWork.SaveChangesAsync();
+                
             }
             else
             {
-
+                result.Message = $@"ENTITY_NOT_EXISTS ""PRODUCT,{dTO.Nombre}""";
+                result.StatusCode = 400;
             }
             return result;
         }
@@ -56,6 +62,21 @@ namespace Restaurante.Services
         public async Task<ResultResponse> Eliminar(string productId)
         {
             var result = new ResultResponse();
+
+            if (_unitOfWork.Producto.ExistsActive(productId))
+            {
+                var currentProduct = _unitOfWork.Producto.WhereActive(x => x.Id == productId).FirstOrDefault();
+                _unitOfWork.Producto.Delete(currentProduct);
+                await _unitOfWork.SaveChangesAsync();
+                result.Message = $@"ENTITY_DELETED ""PRODUCT,{currentProduct.Nombre}""";
+                result.StatusCode = 400;
+            }
+            else
+            {
+                result.Message = $@"ENTITY_NOT_EXISTS_ID ""PRODUCT,{productId}""";
+                result.StatusCode = 400;
+            }
+
             return result;
         }
 

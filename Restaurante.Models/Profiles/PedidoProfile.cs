590a57459b3ba90f15249eaf44d68d88c3a73bdd
Ariel Gallardo <ariel.gallardo.dev@gmail.com>
2024-11-28 09:24:55 -0300

    5-abm-pedido# PedidoDTO Profile - Controller

---
+++ Restaurante.Models/Profiles/PedidoProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace Restaurante.Models.Profiles
+{
+    public class PedidoProfile : Profile
+    {
+        public PedidoProfile()
+        {
+            CreateMap<PedidoDTO.PedidoDTOData, DetallePedido>()
+                .ForMember(x => x.ProductoId, x => x.MapFrom(y => y.ProductoId))
+                .ForMember(x => x.Cantidad, x => x.MapFrom(y => y.Cantidad));
+        }
+    }
+}
--- Restaurante.Services/Implementation/PedidoServices.cs
+++ Restaurante.Services/Implementation/PedidoServices.cs
@@ -1,12 +1,26 @@
-﻿using Restaurante.Models;
+﻿using AutoMapper;
+using Restaurante.Models;
 
 namespace Restaurante.Services
 {
     public class PedidoServices : IPedidoServices
     {
-        public Task<ResultResponse> InteractuarPedidoActual(PedidoDTO dTO)
+        private readonly IMapper _mapper;
+
+        public PedidoServices(IMapper mapper)
         {
-            throw new NotImplementedException();
+            _mapper = mapper;
+        }
+        public async Task<ResultResponse> InteractuarPedidoActual(PedidoDTO dTO)
+        {
+            var result = new ResultResponse();
+            result.StatusCode = 200;
+            result.Message = "On_Development";
+
+            var newStatus = dTO.Estado;
+            var newPedidoList = dTO.Data != null ? _mapper.Map<IList<DetallePedido>>(dTO.Data) : null;
+
+            return result;
         }
     }
 }

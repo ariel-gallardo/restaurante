c15bbd2802de50415662870203048cbf30ddf658
Ariel Gallardo <ariel.gallardo.dev@gmail.com>
2024-11-28 08:57:22 -0300

    5-abm-pedido# PedidoDTO

--- Restaurante.Models/DTO/Pedido/PedidoDTO.cs
+++ Restaurante.Models/DTO/Pedido/PedidoDTO.cs
@@ -2,5 +2,12 @@
 {
     public class PedidoDTO
     {
+        public class PedidoDTOData
+        {
+            public string ProductoId { get; set; }
+            public double Cantidad { get; set; }
+        }
+        public bool Confirmar { get; set; }
+        public IList<PedidoDTOData> Data { get; set; }
     }
 }
